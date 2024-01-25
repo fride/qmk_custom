7e9e4606871b6ed4ed8b51e1a5ea97859f95837d
Jan Friderici <jan@friderici.net>
2024-01-25 09:12:06 +0100

    engram - but kau is worst.

--- user/layout.h
+++ user/layout.h
@@ -192,65 +192,9 @@ enum custom_keycodes {
 #define ___________________________________________                            \
   _______, _______, _______, _______, _______
 
+// clang-format off
 // ----------------
 // layout wrappers inspired by https://github.com/pixelbreaker/qmk_userspace
-
-/*
-MAGIC STURDY
-#define ___A___ LALT_T(KC_A)
-#define ___B___ KC_B
-#define ___C___ KC_C
-#define ___D___ LSFT_T(KC_D)
-#define ___E___ RGUI_T(KC_E)
-#define ___F___ KC_F
-#define ___G___ MEH_T(KC_G)
-#define ___H___ MEH_T(KC_H)
-#define ___I___ RCTL_T(KC_I)
-#define ___J___ KC_J
-#define ___K___ KC_K
-#define ___L___ KC_L
-#define ___M___ KC_M
-#define ___N___ RSFT_T(KC_N)
-#define ___O___ KC_O
-#define ___P___ KC_P
-#define ___Q___ KC_Q
-#define ___R___ LGUI_T(KC_R)
-#define ___S___ LCTL_T(KC_S)
-#define ___T___ LALT_T(KC_T)
-#define ___U___ KC_U
-#define ___V___ KC_V
-#define ___W___ KC_W
-#define ___X___ KC_X
-#define ___Y___ KC_Y
-#define ___Z___ KC_Z
-#define _COMMA_ KC_COMM
-#define __DOT__ KC_DOT
-#define _SLASH_ KC_SLSH
-#define _SQUOT_ KC_QUOT
-#define _DQUOT_ KC_DQUO
-#define _MINUS_ KC_MINS
-#define __HASH_ KC_HASH
-#define _SEMIC_ KC_SCLN
-#define _QUEST_ KC_QUES
-*/
-#define _STURDY                                                                \
-  ___V___, ___M___, ___L___, ___C___, ___P___, ___B___, MAGIC, ___U___,        \
-      ___O___, _SQUOT_, ___S___, ___T___, ___R___, ___D___, ___Y___, ___F___,  \
-      ___N___, ___E___, ___A___, ___I___, ___X___, ___K___, ___J___, ___G___,  \
-      ___W___, ___Z___, ___H___, _COMMA_, __DOT__, KC_QUES, LT(UTIL, KC_BSPC), \
-      NAV_SPC, MAG_ALT, ENT_CTL, OS_LSFT, LT(FUN, KC_DEL)
-
-/*
-  f r d p v  q j u o y
-  s n t c b  . h e a i
-  z x k g w  m l ; ' ,
-*/
-#define _RECURVA                                                               \
-  ___F___, ___R___, ___D___, ___P___, ___V___, _SQUOT_, ___M___, ___U___,      \
-      ___O___, ___Y___, ___S___, ___N___, ___T___, ___C___, ___B___, __DOT__,  \
-      ___H___, ___E___, ___A___, ___I___, ___Z___, ___J___, ___K___, ___G___,  \
-      ___W___, ___X___, ___L___, LPAREN, RPAREN, _COMMA_, TO(ENGRAM), NAV_SPC, \
-      LALT_T(KC_TAB), ENT_CTL, OS_LSFT, KC_Q
 
 /*
 Engram
@@ -264,11 +208,10 @@ Engram
                   TO(ALPHA), NAV_SPC, MAG_ALT,          ENT_CTL,  OS_LSFT, LT(FUN,KC_DEL)
 */
 #define _ENGRAM \
-  ___V___, ___W___, ___D___, ___L___, _SQUOT_ ,        _DQUOT_, ___U___, ___O___, ___Y___, ___B___, \
-  ___N___, ___S___, ___T___, ___H___, _COMMA_,         __DOT__, ___A___, ___E___, ___I___, ___C___, \
-  ___P___, ___F___, ___M___, ___R___, _MINUS_,         _QUEST_, ___K___, ___J___, ___X___, ___G___, \
+  ___B___, ___Y___, ___O___, ___U___, _SQUOT_ ,        _DQUOT_, ___L___, ___D___, ___W___, ___V___, \
+  ___C___, ___I___, ___E___, ___A___, _COMMA_,         __DOT__, ___H___, ___T___, ___S___, ___N___, \
+  ___G___, ___X___, ___J___, ___K___, _MINUS_,         _QUEST_, ___R___, ___M___, ___F___, ___P___, \
                   TO(ALPHA), NAV_SPC, MAG_ALT,          ENT_CTL,  OS_LSFT, LT(FUN,KC_DEL)
-
 
 
 
